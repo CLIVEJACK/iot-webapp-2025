68c6460225be10e7516f84b1667cdbf605e95b9c
DongKuk-Kim <yum0153@gmail.com>
2025-06-10 16:29:55 +0900

    12일차 커밋

---
+++ day12/Day12Study/WpfTodoListApp/Models/TodoItem.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema; // DB스키마 정의 클래스
+using System.Runtime.CompilerServices;
+
+namespace WpfTodoListApp.Models
+{
+    public class TodoItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required] //NotNull 일경우는 string에 ?(Nullable)을 삭제할 것
+        [Column(TypeName = "VARCHAR(100)")] // 이거 사용안하면 컬럼이 LONGTEXT로 생성됨
+        public string Title { get; set; }
+
+        [Required]
+        [Column(TypeName = "CHAR(100)")] // 20250610
+        public string TodoDate { get; set; }
+
+
+        //boolean 
+        public bool IsComplete { get; set; }
+    }
+}
